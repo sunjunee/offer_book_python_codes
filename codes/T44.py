f191d01683d3eff4fc98a09dcb2edbc5823caffc
Jun Sun <sunjunee@qq.com>
2018-05-21 20:23:04 +0800

    change
    change

--- codes/T44.py
+++ codes/T44.py
@@ -9,15 +9,15 @@
 # 数字以01234567891011...的格式序列化到一个字符串序列中，在这个序列中，
 # 第5位是5，第13位是1...请写一个函数，求任意第n位的数字。
 
-def getDigit(num):
+def getDigit(index):
     pre, cur = 0, 1
     digits = 0
-    while(digits + (10 ** cur - 10 ** pre) * cur < num):
+    while(digits + (10 ** cur - 10 ** pre) * cur < index):
         digits += (10 ** cur - 10 ** pre) * cur
         pre, cur = cur, cur + 1
     
-    tem1 = (num - digits) // cur
-    tem2 = (num - digits) % cur
+    tem1 = (index - digits) // cur
+    tem2 = (index - digits) % cur
 
     if(tem2 == 0):  return int(str(10 ** pre + tem1 - 1)[-1])
     return int(str(10 ** pre + tem1)[tem2 - 1])
