f31d95bcbeff6165a7972f4e2ffeabfa3f9c157b
Jun Sun <sunjunee@qq.com>
2018-05-27 21:27:32 +0800

    add
    add

---
+++ codes/T64.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+@ Author: Jun Sun {Python3}
+@ E-mail: sunjunee@qq.com
+@ Date:   2018-05-27 21:06:19
+"""
+
+# T64 求1+2+...+n
+# 求1到n的和，要求不适用乘除法、for、while
+# if、else、switch、case等关键词及条件判断
+# 语句(A?B:C)
+ 
---
+++ codes/T65.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+@ Author: Jun Sun {Python3}
+@ E-mail: sunjunee@qq.com
+@ Date:   2018-05-27 21:08:49
+"""
+
+# T65 不使用加减乘除做加法
+# 写一个函数，计算两个整数的和，不使用加减乘除
+
+# 使用二进制的位运算来求解
+
+def getSum(A, B):
+    
+    while(True):
+        s = A ^ B
+        c = (A & B) << 1
+        A, B = s, c
+        
